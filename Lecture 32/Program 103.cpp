771361737ef48abcc736dd1b4f286be0f1c9e3c9
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-12-19 08:16:40 +0530

    LeetCode 509. Fibonacci Number

---
+++ Lecture 32/Program 103.cpp
@@ -0,0 +1,19 @@
+// LeetCode 509. Fibonacci Number
+//  class Solution {
+//  public:
+//      int fib(int n) {
+
+//         //Base Case
+//         if(n == 0){
+//             return 0;
+//         }
+
+//         if(n == 1){
+//             return 1;
+//         }
+
+//         int ans = fib(n-1)+fib(n-2);
+
+//         return ans;
+//     }
+// };
