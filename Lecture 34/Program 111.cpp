b0cac26761c6803a17a8b4c984c77783979989e5
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-12-21 07:36:11 +0530

    CodeStudio-Reverse the String

---
+++ Lecture 34/Program 111.cpp
@@ -0,0 +1,22 @@
+// CodeStudio-Reverse the String
+// https://www.codingninjas.com/studio/problems/reverse-the-string_799927?leftPanelTab=0&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_13&leftPanelTabValue=PROBLEM
+
+// #include <bits/stdc++.h>
+
+// void reverse(string& str, int i, int j){
+//     //Base case
+//     if(i>j){
+//         return ;
+//     }
+//     swap(str[i],str[j]);
+//     i++;
+//     j--;
+
+//     //Recursive call
+//     reverse(str,i,j);
+// }
+// string reverseString(string str)
+// {
+// 	reverse(str,0,str.length()-1);
+//     return str;
+// }
