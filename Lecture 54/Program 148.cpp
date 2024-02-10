61787543fdff3e9f58a8f6a3e1f8a24eee0a961a
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-02-10 09:10:30 +0530

    Introduction to Stacks

---
+++ Lecture 54/Program 148.cpp
@@ -0,0 +1,35 @@
+// Introduction to Stacks
+#include <iostream>
+#include <stack>
+using namespace std;
+
+// Go to cppreference.com
+
+int main()
+{
+
+    // //Creation of Stack
+    // stack<int> s;
+
+    // // Push operation
+    // s.push(2);
+    // s.push(3);
+
+    // // Pop operation
+    // s.pop();
+
+    // cout << "Printing top element " << s.top() << endl;
+
+    // if (s.empty())
+    // {
+    //     cout << "Stack is empty." << endl;
+    // }
+    // else
+    // {
+    //     cout << "Stack is not empty." << endl;
+    // }
+
+    // cout << "Size of stack is " << s.size() << endl;
+
+    return 0;
+}
