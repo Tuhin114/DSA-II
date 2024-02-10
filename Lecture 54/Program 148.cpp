8fc6485fbc61e50e6334cd6c9ad4281b15d9d6bd
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-02-10 11:48:14 +0530

    Stack operations

--- Lecture 54/Program 148.cpp
+++ Lecture 54/Program 148.cpp
@@ -5,31 +5,103 @@ using namespace std;
 
 // Go to cppreference.com
 
+class Stack
+{
+    // Properties
+public:
+    int *arr;
+    int top;
+    int size;
+
+    // Behaviour
+    Stack(int size)
+    {
+        this->size = size;
+        arr = new int[size];
+        top = -1;
+    }
+
+    void push(int element)
+    {
+        if (size - top > 1)
+        {
+            top++;
+            arr[top] = element;
+        }
+        else
+        {
+            cout << " Stack Overflow " << endl;
+        }
+    }
+
+    void pop()
+    {
+        if (top >= 0)
+        {
+            top--;
+        }
+        else
+        {
+            cout << "Stack  Underflow" << endl;
+        }
+    }
+
+    int peek()
+    {
+        if (top >= 0)
+        {
+            return arr[top];
+        }
+        else
+        {
+            cout << "Stack is Empty" << endl;
+            return -1;
+        }
+    }
+
+    bool isEmpty()
+    {
+        if (top == -1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
 int main()
 {
+    Stack st(5);
 
-    // //Creation of Stack
-    // stack<int> s;
+    st.push(22);
+    st.push(43);
+    st.push(44);
+    st.push(22);
+    st.push(43);
+    st.push(44);
 
-    // // Push operation
-    // s.push(2);
-    // s.push(3);
+    cout << st.peek() << endl;
 
-    // // Pop operation
-    // s.pop();
+    st.pop();
+    cout << st.peek() << endl;
 
-    // cout << "Printing top element " << s.top() << endl;
+    st.pop();
+    cout << st.peek() << endl;
 
-    // if (s.empty())
-    // {
-    //     cout << "Stack is empty." << endl;
-    // }
-    // else
-    // {
-    //     cout << "Stack is not empty." << endl;
-    // }
+    st.pop();
+    cout << st.peek() << endl;
 
-    // cout << "Size of stack is " << s.size() << endl;
+    if (st.isEmpty())
+    {
+        cout << "Stack  is empty" << endl;
+    }
+    else
+    {
+        cout << "Stack is not empty" << endl;
+    }
 
     return 0;
 }
