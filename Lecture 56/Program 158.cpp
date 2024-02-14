1be4e7eedfabf882af791a1cbeab8030865ccada
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-02-14 12:02:58 +0530

    CodeStudio -  Next Smaller Element

---
+++ Lecture 56/Program 158.cpp
@@ -0,0 +1,23 @@
+// CodeStudio -  Next Smaller Element
+// https://www.codingninjas.com/studio/problems/next-smaller-element_1112581?topList=love-babbar-dsa-sheet-problems&leftPanelTab=0%3Fsource%3Dyoutube&campaign=Lovebabbarcodestudio&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbarcodestudio&leftPanelTabValue=PROBLEM
+
+#include <stack>
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    stack<int> s;
+    s.push(-1);
+    vector<int> ans(n);
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int curr = arr[i];
+        while (s.top() >= curr)
+        {
+            s.pop();
+        }
+        // Ans is top of stack
+        ans[i] = s.top();
+        s.push(curr);
+    }
+    return ans;
+}
