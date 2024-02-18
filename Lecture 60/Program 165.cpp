99a9baed03f4057f33ed24f2c96fa28e2347b196
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-02-18 08:42:19 +0530

    CodeStudio -  Implement a Queue

---
+++ Lecture 60/Program 165.cpp
@@ -0,0 +1,81 @@
+// CodeStudio -  Implement a Queue
+//  https://www.codingninjas.com/studio/problems/queue-using-array-or-singly-linked-list_2099908?leftPanelTab=0%3Fsource%3Dyoutube&campaign=LoveBabbar_Codestudio&utm_source=youtube&utm_medium=affiliate&utm_campaign=LoveBabbar_Codestudio&leftPanelTabValue=PROBLEM
+
+#include <bits/stdc++.h>
+class Queue
+{
+
+    int *arr;
+    int qfront;
+    int rear;
+    int size;
+
+public:
+    Queue()
+    {
+        // Implement the Constructor
+        size = 100001;
+        arr = new int[size];
+        qfront = 0;
+        rear = 0;
+    }
+
+    /*----------------- Public Functions of Queue -----------------*/
+
+    bool isEmpty()
+    {
+        if (qfront == rear)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void enqueue(int data)
+    {
+        if (rear == size)
+        {
+            cout << "Queue is Full" << endl;
+        }
+        else
+        {
+            arr[rear] = data;
+            rear++;
+        }
+    }
+
+    int dequeue()
+    {
+        if (qfront == rear)
+        {
+            return -1;
+        }
+        else
+        {
+            int ans = arr[qfront];
+            arr[qfront] = -1;
+            qfront++;
+            if (qfront == rear)
+            {
+                qfront = 0;
+                rear = 0;
+            }
+            return ans;
+        }
+    }
+
+    int front()
+    {
+        if (qfront == rear)
+        {
+            return -1;
+        }
+        else
+        {
+            return arr[qfront];
+        }
+    }
+};
